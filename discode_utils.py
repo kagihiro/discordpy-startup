2894b9ab0eac37dfdef0b30e354c462b72307c6d
鎰廣匡佑 <nkc.kyousuke.kagihiro@gmail.com>
2021-01-19 00:17:57 +0900

    a

--- discode_utils.py
+++ discode_utils.py
@@ -3,8 +3,7 @@ from discord.ext import commands
 
 def get_mention(ctx):
     member_id = ctx.author.id
-    member = ctx.guild.get_member(member_id)
-    return member.mention
+    return f"<@!{member_id}>"
 
 
 def get_mentioned_message(ctx, message):
